89d4d06795fdffa1ee35f3ed78b83df7d974edda
Smruti Ranjan <smrutisahoo7014@gmail.com>
2025-05-22 11:21:26 +0530

    Add files via upload

---
+++ Rotate_Array.java
@@ -0,0 +1,27 @@
+public class Rotate_Array {
+    public static void rotateArr(int[] arr , int d){
+        int n = arr.length;
+        reverse(arr, 0, d-1);
+        reverse(arr, d, n-1);
+        reverse(arr, 0, n-1);
+    }
+
+    public static void reverse(int[] arr, int start , int end){
+        while (start < end) {
+            int temp = arr[start];
+            arr[start]= arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6,7,8};
+        int n = 3;
+        rotateArr(arr, n);
+        for (int obj : arr) {
+            System.out.print(obj +" ");
+        }
+    }
+}
