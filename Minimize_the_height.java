0b05b617e7f8df50d9981d2912b79faacbb9baf5
Smruti Ranjan <smrutisahoo7014@gmail.com>
2025-05-26 22:58:45 +0530

    Add files via upload

---
+++ Minimize_the_height.java
@@ -0,0 +1,30 @@
+// User function Template for Java
+import java.util.Arrays;
+class Solution {
+     int getMinDiff(int[] arr, int k) {
+       int n = arr.length;
+       Arrays.sort(arr);
+       int res = arr[n-1] - arr[0];
+       
+       for(int i = 1 ; i < n ;i++ )
+       {
+           if (arr[i] - k < 0)
+           continue;
+           
+            int minH = Math.min(arr[0]+k , arr[i]-k);
+            int maxH = Math.max(arr[i-1]+k , arr[n-1]-k);
+
+            res = Math.min(res,maxH - minH);
+            
+       }
+       
+       return res;
+    }
+    public static void main(String[]args){
+        int []arr = {1,5,8,10};
+        int k = 2;
+        Solution ob = new Solution();
+        int ans = ob.getMinDiff(arr , k );
+        System.out.println(ans);
+    }
+}
